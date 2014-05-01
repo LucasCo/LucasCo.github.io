11a9cff4a56e66a29718706b87fdb9f90cfa5a2d
Luke Hedge <hedge.luke@gmail.com>
2014-05-01 18:14:05 +1000

    added rainfall data to the use_spatialpointsdataframe, in the the actual read file

--- R/data_read_in_est_and_use.R
+++ R/data_read_in_est_and_use.R
@@ -14,7 +14,9 @@ i <- sapply(use_spdf@data, is.factor)###convert factors to characters
 use_spdf@data[i] <- lapply(use_spdf@data[i], as.character)
 rm(i)
 
-use_df<-use_spdf@data
+names(use_spdf@data)[names(use_spdf@data)=="date_1"] <- "Date"
+
+
 
 ######
 #Create new entries for the value of 'fleet size'
@@ -31,4 +33,28 @@ use_df$INT<-as.character(interaction(use_df$Season,use_df$DyTrns_, use_df$date_1
 use_spdf$date_1<-as.Date(use_spdf$date_1,format='%Y-%m-%d')
 use_df$date_1<-as.Date(use_df$date_1,format='%Y-%m-%d')
 
+
+#######
+#RAINFALL DATA
+#######
+#From BOM.
+
+#read in rainfall dat
+rain<-read.csv("Data/rainfall_data.csv", header=T, stringsAsFactors=FALSE)
+rain<-rain[,3:6]
+colnames(rain)[4]<-"Rainfall"
+
+#subset to year 2013:2013
+rain<-rain[rain$Year>=2013,]
+
+rain$Date<-as.character(interaction(rain$Day,rain$Month,rain$Year,sep=":"))
+rain$Date<-as.Date(rain$Date,format='%d:%m:%Y')
+
+######MERGE rainfall and use_spdf
+use_spdf@data<-merge(use_spdf@data,rain)
+
+
+#####
+#TIDE DATA
+#####
 
